4042de117e48656cb8ee501bf9bae5c245a54c69
Skovgaard <masr@bws.dk>
2019-01-31 23:12:45 +0100

    Styling setup
    HTML, CSS, Bootstrap

---
+++ src/main/java/com/example/springbootfun/controllers/ViewController.java
@@ -0,0 +1,20 @@
+package com.example.springbootfun.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Skovgaard");
+        model.addAttribute("mode", "production");
+
+        return "index";
+    }
+}
