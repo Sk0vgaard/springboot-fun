70b57baa19b460ecf36c2e23e8ff4355d59505bd
Skovgaard <masr@bws.dk>
2019-02-01 20:06:18 +0100

    Added JSONDoc
    url: http://localhost:8080/jsondoc-ui.html
    
    input: http://localhost:8080/jsondoc

--- src/main/java/com/example/springbootfun/controllers/BookingController.java
+++ src/main/java/com/example/springbootfun/controllers/BookingController.java
@@ -2,6 +2,10 @@ package com.example.springbootfun.controllers;
 
 import com.example.springbootfun.models.TreatmentBooking;
 import com.example.springbootfun.repositories.BookingRepository;
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.pojo.ApiStage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/bookings")
+@Api(name = "Treatment Booking API", description = "Provides a list of methods that manage treatment bookings", stage = ApiStage.RC)
 public class BookingController {
 
     private BookingRepository bookingRepository;
@@ -19,18 +24,20 @@ public class BookingController {
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ApiMethod(description = "Get all treatment bookings from the database")
     public List<TreatmentBooking> getAll() {
         return bookingRepository.findAll();
     }
 
-
     /* Filter to show prices equals or below fx 300. */
     @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
-    public List<TreatmentBooking> getAffordable(@PathVariable double price) {
+    @ApiMethod(description = "Get all treatments where the price is less or equal than the provided value")
+    public List<TreatmentBooking> getAffordable(@ApiPathParam(name = "price") @PathVariable double price) {
         return bookingRepository.findByPriceForTreatmentLessThan(price);
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ApiMethod(description = "Create a treatment and saves it to the database")
     public List<TreatmentBooking> create(@RequestBody TreatmentBooking treatmentBooking) {
         bookingRepository.save(treatmentBooking);
 
@@ -38,7 +45,8 @@ public class BookingController {
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    public List<TreatmentBooking> delete(@PathVariable long id) {
+    @ApiMethod(description = "Deletes the treatment booking with the provided ID from the database")
+    public List<TreatmentBooking> delete(@ApiPathParam(name = "id") @PathVariable long id) {
         bookingRepository.deleteById(id);
 
         return bookingRepository.findAll();
